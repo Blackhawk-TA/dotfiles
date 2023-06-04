384f29639a1633c5402377e2d97771358a9c489e
Blackhawk <daniel.peters1@pm.me>
2023-06-04 21:47:39 +0200

    Rename .zshrc script

--- zsh/setup-zsh.sh
+++ zsh/setup-zsh.sh
@@ -31,7 +31,7 @@ else
 fi
 
 echo "Applying .zshrc from configs directory"
-ZSH_RC_PATH=$SCRIPT_DIR/configs/zshrc-config
+ZSH_RC_PATH=$SCRIPT_DIR/configs/dot-zshrc
 if [ ! -f "$ZSH_RC_PATH" ]; then
 	echo ".zshrc config not found in configs directory, exiting..."
 	exit 1
