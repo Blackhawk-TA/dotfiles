ae47d4c6d358e5426d149d7d02b73a76a21cb0e3
Blackhawk <daniel.peters1@pm.me>
2023-06-04 16:55:25 +0200

    Use arch packages for nerdfont installation

--- setup-neovim.sh
+++ setup-neovim.sh
@@ -12,11 +12,8 @@ echo "Installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "Installing nerd-fonts"
-cd tmp
-git clone https://github.com/ryanoasis/nerd-fonts.git
-/bin/sh $SCRIPT_DIR/tmp/nerd-font/install.sh
-cd $SCRIPT_DIR
+echo "Installing fonts"
+sudo pacman -Sy ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
 
 echo "Downloading nvim config"
 cd tmp
