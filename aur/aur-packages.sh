93877f2f7bf44f956b5490e62dd4f065a25d09ed
Blackhawk <daniel.peters1@pm.me>
2024-05-23 09:44:15 +0200

    Update aur packages

--- aur/aur-packages.sh
+++ aur/aur-packages.sh
@@ -20,13 +20,15 @@ git clone https://aur.archlinux.org/edmarketconnector.git
 git clone https://aur.archlinux.org/golangci-lint.git
 git clone https://aur.archlinux.org/imagewriter.git
 git clone https://aur.archlinux.org/jetbrains-toolbox.git
+git clone https://aur.archlinux.org/minecraft-launcher.git
 git clone https://aur.archlinux.org/nvm.git
 git clone https://aur.archlinux.org/postman-bin.git
+git clone https://aur.archlinux.org/proton-ge-custom-bin.git
 git clone https://aur.archlinux.org/protontricks.git
 git clone https://aur.archlinux.org/tidal-hifi-bin.git
 git clone https://aur.archlinux.org/unixbench.git
-git clone https://aur.archlinux.org/xone-dongle-firmware.git
 git clone https://aur.archlinux.org/xone-dkms-git.git
+git clone https://aur.archlinux.org/xone-dongle-firmware.git
 
 echo "Installing aur packages"
 for dir in */; do
