df4d744cf82e7a9a488457b10ee7435b0b8497d6
Blackhawk <daniel.peters1@pm.me>
2025-01-09 10:11:11 +0100

    Add vi mode plugin to zsh config

--- zsh/setup-zsh.sh
+++ zsh/setup-zsh.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "Installing dependencies"
 sudo pacman -Sy archlinux-keyring --noconfirm --needed
 sudo pacman -Syu --noconfirm --needed
-sudo pacman -Sy curl --noconfirm --needed
+sudo pacman -Sy curl git --noconfirm --needed
 
 echo "Generate locale en_US.UTF-8"
 sudo locale-gen en_US.UTF-8
@@ -30,13 +30,22 @@ else
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+echo "Installing zsh plugins on system"
+AUR_DIR=$HOME/aur-packages
+mkdir -p $AUR_DIR
+git clone https://aur.archlinux.org/zsh-vi-mode.git $AUR_DIR/zsh-vi-mode
+cd $AUR_DIR/zsh-vi-mode || echo "Failed to install zsh plugins using aur"
+makepkg -si --noconfirm
+cd $SCRIPT_DIR || exit 1
+
 echo "Applying .zshrc from configs directory"
 ZSH_RC_PATH=$SCRIPT_DIR/configs/dot-zshrc
 if [ ! -f "$ZSH_RC_PATH" ]; then
 	echo ".zshrc config not found in configs directory, exiting..."
 	exit 1
 fi
-cp $ZSH_RC_PATH /home/$USER/.zshrc
+cp $ZSH_RC_PATH $HOME/.zshrc
+source $HOME/.zshrc
 
 echo "zsh setup completed, reload terminal to see effects"
 
