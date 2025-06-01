514dd6bd0188b70e5a9559ae03c6da076e3ab56b
Blackhawk <daniel.peters1@pm.me>
2025-06-01 23:15:04 +0200

    Improve aur cleanup script

--- aur/scripts/cleanup.sh
+++ aur/scripts/cleanup.sh
@@ -3,13 +3,13 @@
 for dir in */; do
 	echo "Cleaning up $dir"
 	cd "$dir" || exit 1
-	if [ `ls -1 ./*.pkg.tar.zst 2>/dev/null | wc -l` -gt 0 ]; then
+	if [ "$(ls -1 ./*.pkg.tar.zst 2>/dev/null | wc -l)" -gt 0 ]; then
 		rm ./*.pkg.tar.zst
 	fi
-	if [ `ls -1 ./*.tar.gz 2>/dev/null | wc -l` -gt 0 ]; then
+	if [ "$(ls -1 ./*.tar.gz 2>/dev/null | wc -l)" -gt 0 ]; then
 		rm ./*.tar.gz
 	fi
-	if [ `ls -1 ./*.deb 2>/dev/null | wc -l` -gt 0 ]; then
+	if [ "$(ls -1 ./*.deb 2>/dev/null | wc -l)" -gt 0 ]; then
 		rm ./*.deb
 	fi
 	cd ..
