28f783a2f06fbca2896f8c9298bdeeb02f830747
Blackhawk <daniel.peters1@pm.me>
2025-07-28 23:58:36 +0200

    Add packages for 3D printing

--- packages/install-packages.sh
+++ packages/install-packages.sh
@@ -9,6 +9,8 @@ sudo pacman -Sy teamspeak3 steam steam-native-runtime --noconfirm --needed
 echo "Installing development related packages"
 sudo pacman -Sy tiled vi go nvm --noconfirm --needed
 
+echo "Installing 3D printing related packages"
+sudo pacman -Sy prusa-slicer freecad --noconfirm --needed
+
 echo "Installing utility packages"
 sudo pacman -Sy firefox tree signal-desktop nextcloud-client vlc bind wget unzip fzf filelight --noconfirm --needed
-
